804139a2e48c3a37600f1a862a7a2b336678c3ce
jalpa <jalpalalwani2000@gmail.com>
2024-03-22 15:38:28 +0500

    Simple CLI Calculator

---
+++ index.ts
@@ -0,0 +1,41 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+//Printing a wellcome Message
+
+console.log("\n\tWellCome to \'CodeWithjalpa\' - CLI Simple Calculator\n");
+
+// Asking Questions from Users through Inquirer
+
+let answers = await inquirer.prompt([
+    {message: "Enter First Number", type: "number", name: "firstNumber" },
+    {message: "Enter Second Number", type: "number", name: "secondNumber"},
+  {
+    message: "Select one Operator to perform Operations",
+    type: "list",
+    name: "operator",
+    choices:["Addition", "Subtraction","Multiplication","Division", "Persantage"],
+
+  },
+]);
+// console.log(answers);
+//conditional statement if-else
+if(answers.operator === "Addition"){
+  console.log(answers.firstNumber + answers.secondNumber)
+}
+else if(answers.operator === "Subtraction"){
+ console.log(answers.firstNumber - answers.secondNumber)
+}
+else if(answers.operator === "Multiplication"){
+    console.log(answers.firstNumber * answers.secondNumber)
+   }
+   else if(answers.operator === "Division"){
+    console.log(answers.firstNumber / answers.secondNumber)
+   }
+   else if(answers.operator === "Persantage"){
+    console.log(answers.firstNumber % answers.secondNumber)
+   }
+   else{
+    ("Invalid Input")
+   };
